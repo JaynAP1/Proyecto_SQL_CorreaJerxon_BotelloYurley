f2f5af18c71f875b1575dc92b1f7f21b1b9dd536
JaynAp1 <jerxonmjair@gmail.com>
2024-11-26 12:45:15 -0500

    :sparkles: New querys to total 50

--- dql_select.sql
+++ dql_select.sql
@@ -63,7 +63,8 @@ select concat_ws("",Nombre1," ", Nombre2," ", Apellido1, " ",Apellido2) as Nombr
 -- 21. Muestra todos los clientes que vivan en bogota.
 select * from clientes where Direccion like "%Bogota%";
 
--- 22. Muestre todos los clientes que su cedula inicie
+-- 22. Muestre todos los clientes que su cedula inicie en 1 y termine en 5.
+select Nombre1, Cedula from clientes having left(Cedula, 1) = 1 and right(Cedula,1) = 5;
 
 -- 23. Muestre todas las empresas.
 select * from empresa;
@@ -73,3 +74,79 @@ select * from empresa where Ubicacion = "Bogota";
 
 -- 25. Muestre las empresas que tengan que ver con fertilizantes.
 select * from empresa where Descripcion like '%fertilizantes%';
+
+-- 26. Muestre Todas las empresas que en su nombre contengan la palabra "Agro".
+select * from empresa where Nombre like '%Agro%';
+
+-- 27. Muestre la cantidad de empresas que tienen la palabra "Agro".
+Select count(Nombre) as Empresas_Agro from Empresa where Nombre like '%Agro%';
+
+-- 28. Muestre las empresas que no tienen la palabra "Agro".
+Select * from Empresa where Nombre not like '%Agro%';
+
+-- 29. Muestre todos los estados que hay.
+select * from estado;
+
+-- 30. Mostrar el historial.
+select * from historial;
+
+-- 31. Mostrar todo el historial que tengan de tipo mantenimiento.
+select * from historial where Tipo = "Mantenimiento";
+
+-- 32. Mostrar la cantidad de objetos del historial que esta en cosecha.
+select count(Tipo) from historial where Tipo = "Cosecha";
+
+-- 33. Mostrar los historiales que tienen que ver con cacao.
+select * from historial where Descripcion like "%Cacao%";
+
+-- 34. Mostrar el nombre de las fincas que tienen historial.
+select historial.Tipo, historial.Descripcion, finca.Nombre from historial inner join Finca on historial.id_finca = finca.id;
+
+-- 35. Mostrar todos los fertilizantes almacenados.
+select * from inventario_produccion where Nombre like '%Fertilizante%';
+
+-- 36. Mostrar los tipos de cafe almacenados y sus objetos.
+select * from inventario_produccion where Nombre like '%Cafe%';
+
+-- 37. Mostrar el cacao y sus objetos almacenados.
+select * from inventario_produccion where Nombre like '%Cacao%';
+
+-- 38. Mostrar el nombre de los productos que no son traidos por proveedores.
+select nombre from inventario_produccion where id_Proveedor is null;
+
+-- 39. Mostrar el nombre de los proveedores que cada objeto.
+select inventario_produccion.Nombre, concat_ws("",proveedores.Nombre," ", Apellido1, " ",Apellido2) as Nombre_Completo  from inventario_produccion inner join proveedores on inventario_produccion.id_proveedor = proveedores.id;
+
+-- 40. Mostrar el objeto con el valor mas alto.
+select * from inventario_produccion order by 4 desc;
+
+-- 41. Sacar el promedio de todos los objetos referentes al cafe.
+select avg(Valor) as promedio_Cafe from (select sum(Valor_Unitario) as "Valor" from inventario_produccion where Nombre like "%Cafe%") as obtener;
+
+-- 42. Sacar el promedio de todos los fertilizantes. 
+select avg(Valor) as promedio_Cafe from (select sum(Valor_Unitario) as "Valor" from inventario_produccion where Nombre like "%Fertilizante%") as obtener;
+
+-- 43. Sacar el promedio de todos los objetos.
+select avg(Valor) as promedio_Cafe from (select sum(Valor_Unitario) as "Valor" from inventario_produccion) as obtener;
+
+-- 44. Sacar el valor total de cada objeto.
+select Nombre, Valor from (select Nombre as "Nombre", (cantidad * valor_unitario) as "Valor" from inventario_produccion) as obtener;
+
+-- 45. Sacar el valor total de todos los objetos.
+select Valor from (select sum(Cantidad * valor_unitario) as "Valor" from inventario_produccion) as obtener;
+
+-- 46. Mostrar la tabla inventario_producto.
+select * from inventario_producto;
+
+-- 47. Mostrar los productos que vencen en el 2025 y mes 6.
+select * from (select * from inventario_producto where month(Fecha_Caducidad) = 6) as obtener;
+
+-- 48. Mostrar los productos que fueron producion en el mes 1 del año 2024.
+select * from (select * from inventario_producto where month(Fecha_Produccion) = 1) as obtener;
+
+-- 49. Mostrar los productos con mas stock de 1000.
+select * from (select * from inventario_producto where Cantidad_Stock > 1000) as obtener; 
+
+-- 50. Mostrar el nombre de los productos.
+select id, Nombre, Cantidad from (select Inventario_producto.id as "id",Cantidad_stock as "Cantidad" ,productos.Nombre as "Nombre" from Inventario_producto inner join productos on inventario_producto.id = productos.id_inventario_producto) as obtener;
+
